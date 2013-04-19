1339e566a5abb5be06472941e6e1553923e0b452
Yang Han <hanyang213@gmail.com>
2013-04-19 22:41:10 +0800

    prepare for identify whole

--- weibo/identify/views.py
+++ weibo/identify/views.py
@@ -23,11 +23,57 @@ def index():
 
 @mod.route("/whole/", methods=["GET", "POST"])
 def whole():
-    return render_template('identify/whole.html')
+    request_method = request.method
+    if request_method == 'GET':
+        return render_template('identify/whole.html', from_external=True)
+    elif request_method == 'POST':
+        form = request.form
+        action = form.get('action', 'check')
+
+        top_n = form.get('top_n', 2000)
+        page_num = form.get('page_num', 20)
+        rank_method = form.get('rank_method', 'followers')
+        window_size = form.get('window_size', 1)
+
+        if action == 'rank':
+            data = read_current_results(top_n, topic_id=None, r='whole', m=rank_method, w=window_size)
+            return json.dumps({'status': 'current finished', 'data': data})
+        elif action == 'previous_rank':
+            previous_data = read_previous_results(top_n, topic_id=None, r='area', m=rank_method, w=window_size)
+            return json.dumps({'status': 'previous finished', 'data': previous_data})
+        elif action == 'run':
+            return render_template('identify/whole.html', top_n=top_n, page_num=page_num)
+        else:
+           abort(404)
+    else:
+        abort(404)
 
 @mod.route("/burst/", methods=["GET", "POST"])
 def burst():
-    return render_template('identify/burst.html')
+    request_method = request.method
+    if request_method == 'GET':
+        return render_template('identify/burst.html', from_external=True)
+    elif request_method == 'POST':
+        form = request.form
+        action = form.get('action', 'check')
+
+        top_n = form.get('top_n', 2000)
+        page_num = form.get('page_num', 20)
+        rank_method = form.get('rank_method', 'comments')
+        window_size = form.get('window_size', 1)
+
+        if action == 'rank':
+            data = read_current_results(top_n, topic_id=None, r='burst', m=rank_method, w=window_size)
+            return json.dumps({'status': 'current finished', 'data': data})
+        elif action == 'previous_rank':
+            previous_data = read_previous_results(top_n, topic_id=None, r='burst', m=rank_method, w=window_size)
+            return json.dumps({'status': 'previous finished', 'data': previous_data})
+        elif action == 'run':
+            return render_template('identify/burst.html', top_n=top_n, page_num=page_num)
+        else:
+           abort(404)
+    else:
+        abort(404)
 
 @mod.route("/area/", methods=["GET", "POST"])
 def area():
@@ -37,57 +83,50 @@ def area():
     elif request_method == 'POST':
         form = request.form
         action = form.get('action', 'check')
+
         field = form.get('field', None)
         keywords = form.get('keywords', None)
         top_n = form.get('top_n', 2000)
         page_num = form.get('page_num', 20)
+        window_size = form.get('window_size', 1)
 
-        demo = form.get('demo', 0)
+        if window_size == 1:
+            rank_method = 'pagerank'
+        else:
+            rank_method = 'degree'
+
+        hadoop = int(form.get('hadoop', 0))
 
         field_id = acquire_id('Field', field)
         topic_id = acquire_id('Topic', keywords)
         
-        #just for demo without Hadoop
-        if demo:
-            if action == 'rank':
-                data = read_current_results(topic_id, top_n, demo=True)
-                return json.dumps({'status': 'current finished', 'data': data})
-            elif action == 'previous_rank':
-                previous_data = read_previous_results(topic_id, top_n)
-                return json.dumps({'status': 'previous finished', 'data': previous_data})
-            elif action == 'run':
-                if not keywords:
-                    flash(u'请输入关键词！')
-                    return render_template('identify/area.html', demo=True, from_external=True)
-                
-                return render_template('identify/area.html', field=field, keywords=keywords, top_n=top_n, page_num=page_num, demo=True)
-            else:
-                abort(404)
-
-        #Hadoop
-        job_id = generate_job_id(topic_id)
-        iter_count = 3
-        if action == 'check':
+        if action == 'rank':
+            data = read_current_results(top_n, topic_id=topic_id, r='area', m=rank_method, w=window_size)
+            return json.dumps({'status': 'current finished', 'data': data})
+        elif action == 'previous_rank':
+            previous_data = read_previous_results(top_n, topic_id=topic_id, r='area', m=rank_method, w=window_size)
+            return json.dumps({'status': 'previous finished', 'data': previous_data})
+        elif action == 'check':
             #check Hadoop Job Status
+            job_id = generate_job_id(topic_id)
             status = monitor(job_id)
             if status == 'finished':
-                data = read_current_results(topic_id, top_n)
+                data = hadoop_results(topic_id, top_n,)
                 if data == 'results_not_prepared':
                     return json.dumps({'status': 'results_not_prepared'})
                 return json.dumps({'status': status, 'data': data})
             else:
                 return json.dumps({'status': status})
-        elif action == 'run':
-            #launch Hadoop Job
-            if not keywords:
-                flash(u'请输入关键词！')
-                return render_template('identify/area.html', from_external=True)
+        elif action == 'hadoop':
+            job_id = generate_job_id(topic_id)
+            iter_count = 3
+            
 
             #prepare for network data
             tmp_file = prepare_data(field, topic)
             input_tmp_path = tmp_file.name
-
             print 'prepare Hadoop Job %s ...' % job_id
+            #launch Hadoop Job
             pagerank_cmd = 'python %s/weibo/identify/pagerank.py -j %s -i %s -c %s' % (os.getcwd(), job_id, input_tmp_path, iter_count)
             print 'Command %s ' % pagerank_cmd
             process = subprocess.Popen(pagerank_cmd, 
@@ -98,7 +137,12 @@ def area():
 
             print 'Hadoop Job %s has been launched!' % job_id
             tmp_file.close()
-            return render_template('identify/area.html', field=field, keywords=keywords, top_n=top_n, page_num=page_num)
+            return json.dumps({'status': 'running'})
+        elif action == 'run':
+            if not keywords:
+                flash(u'请输入关键词！')
+                return render_template('identify/area.html', hadoop=hadoop, from_external=True)
+            return render_template('identify/area.html', field=field, keywords=keywords, top_n=top_n, page_num=page_num, hadoop=hadoop)
         else:
             abort(404)
     else:

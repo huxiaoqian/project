151930e3171dcb1bd74a78cabc1fb435ef2f0079
Yang Han <hanyang213@gmail.com>
2013-04-23 11:51:01 +0800

    set web debug false

--- run.py
+++ run.py
@@ -8,8 +8,5 @@ optparser = OptionParser()
 optparser.add_option('-p', '--port', dest='port', help='Server Http Port Number', default=9001, type='int')
 (options, args) = optparser.parse_args()
 
-if not options.port:
-    print 'Usage: python run.py --help'
-
 app = create_app()
-app.run(host='0.0.0.0', port=options.port, debug=True)
+app.run(host='0.0.0.0', port=options.port)

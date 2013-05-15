d614344382fc4ddcbbbb384accbbc35baa236aec
MOON_CLJ <lijunli2598@gmail.com>
2013-05-15 21:28:49 +0800

    fix ../moodlens/views.py

--- weibo/moodlens/views.py
+++ weibo/moodlens/views.py
@@ -1,25 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, render_template
-import leveldb
+from xapian_weibo.xapian_backend import XapianSearch
 import simplejson as json
 import datetime
 import time
-import os
 
 mod = Blueprint('moodlens', __name__, url_prefix='/moodlens')
-LEVELDBPATH = '/home/mirage/leveldb'
-buckets = {}
-
-
-def get_bucket(bucket):
-    if bucket in buckets:
-        return buckets[bucket]
-    buckets[bucket] = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'lijun_' + bucket),
-                                      block_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
-    return buckets[bucket]
 
 emotions_kv = {'happy': 1, 'angry': 2, 'sad': 3}
+s = XapianSearch(path='/opt/xapian_weibo/data/', name='master_timeline_sentiment')
 
 
 @mod.route('/')
@@ -30,21 +20,22 @@ def index():
 @mod.route('/data/<emotion>/')
 def data(emotion):
     data = []
-    total_days = 89
+    total_days = 90
 
     today = datetime.datetime.today()
     now_ts = time.mktime(datetime.datetime(today.year, today.month, today.day, 2, 0).timetuple())
     now_ts = int(now_ts)
     during = 24 * 3600
 
-    bucket = get_bucket('weibo_daily_sentiment_count')
     for i in xrange(-total_days + 1, 1):
-        lt = now_ts + during * i
-        try:
-            daily_emotion_count = bucket.Get(str(lt) + '_' + str(emotions_kv[emotion]))
-            daily_emotion_count = int(daily_emotion_count)
-        except KeyError:
-            daily_emotion_count = 0
-        data.append([lt * 1000, daily_emotion_count])
+        begin_ts = now_ts + during * (i - 1)
+        end_ts = now_ts + during * i
+        query_dict = {
+            'timestamp': {'$gt': begin_ts, '$lt': end_ts},
+            'sentiment': emotions_kv[emotion],
+        }
+        count, _ = s.search(query=query_dict)
+
+        data.append([end_ts * 1000, count])
 
     return json.dumps(data)

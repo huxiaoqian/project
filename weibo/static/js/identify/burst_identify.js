3ed1184648bec7887150ae7fe1f899cf388b1519
Yang Han <hanyang213@gmail.com>
2013-05-23 13:37:55 +0800

    add identify list pagination

--- weibo/static/js/identify/burst_identify.js
+++ weibo/static/js/identify/burst_identify.js
@@ -1,3 +1,5 @@
+var previous_data = null;
+var current_data = null;
 (function ($) {
     function request_callback(data) {
 	var status = data['status'];
@@ -6,9 +8,34 @@
 	    $("#current_process_bar").css('width', "100%")
 	    $("#current_process").removeClass("active");
 	    $("#current_process").removeClass("progress-striped");
-	    if (data.length) {
+	    current_data = data;
+	    if (current_data.length) {
 		$("#loading_current_data").text("计算完成!");
-		create_current_table(data, 20);
+		if (current_data.length < page_num) {
+		    page_num = current_data.length
+		    create_current_table(current_data, 0, page_num);
+		}
+		else {
+		    create_current_table(current_data, 0, page_num);
+		    var total_pages = 0;
+		    if (current_data.length % page_num == 0) {
+			total_pages = current_data.length / page_num;
+		    }
+		    else {
+			total_pages = current_data.length / page_num + 1;
+		    }
+		    $('#rank_page_selection').bootpag({
+			total: total_pages,
+			page: 1,
+			maxVisible: 30
+		    }).on("page", function(event, num){
+			start_row = (num - 1)* page_num;
+			end_row = start_row + 20;
+			if (end_row > current_data.length)
+			    end_row = current_data.length;
+			create_current_table(current_data, start_row, end_row);
+		    });
+		}
 	    }
 	    else {
 		$("#loading_current_data").text("本期计算结果为空!");
@@ -22,9 +49,34 @@
 	    $("#previous_process_bar").css('width', "100%")
 	    $("#previous_process").removeClass("active");
 	    $("#previous_process").removeClass("progress-striped");
-	    if (data.length) {
+	    previous_data = data;
+	    if (previous_data.length) {
 		$("#loading_previous_data").text("计算完成!");
-		create_previous_table(data, 20);
+		if (previous_data.length < page_num) {
+		    page_num = previous_data.length
+		    create_previous_table(previous_data, 0, page_num);
+		}
+		else {
+		    create_previous_table(previous_data, 0, page_num);
+		    var total_pages = 0;
+		    if (previous_data.length % page_num == 0) {
+			total_pages = previous_data.length / page_num;
+		    }
+		    else {
+			total_pages = previous_data.length / page_num + 1;
+		    }
+		    $('#previous_rank_page_selection').bootpag({
+			total: total_pages,
+			page: 1,
+			maxVisible: 30
+		    }).on("page", function(event, num){
+			start_row = (num - 1)* page_num;
+			end_row = start_row + 20;
+			if (end_row > previous_data.length)
+			    end_row = previous_data.length;
+			create_previous_table(previous_data, start_row, end_row);
+		    });
+		}
 	    }
 	    else {
 		$("#loading_previous_data").text("上期结果不存在!");
@@ -34,12 +86,12 @@
 	    return
     }
     
-    function create_current_table(data, rowCount) {
+    function create_current_table(data, start_row, end_row) {
 	var cellCount = 9;
 	var table = '<table class="table table-bordered">';
 	var thead = '<thead><tr><th>排名</th><th>博主ID</th><th>博主昵称</th><th>博主地域</th><th>博主微博</th><th>转发数</th><th>评论数</th><th>同比</th><th>敏感状态</th><th>全选<input type="checkbox"></th></tr></thead>';
 	var tbody = '<tbody>';
-	for(var i = 0;i < rowCount;i++) {
+	for (var i = start_row;i < end_row;i++) {
             var tr = '<tr>';
 	    if (data[i][3].match("海外")) {
 		tr = '<tr class="success">';
@@ -88,12 +140,12 @@
 	$("#rank_table").html(table);
     }
 
-    function create_previous_table(data, rowCount) {
+    function create_previous_table(data, start_row, end_row) {
 	var cellCount = 7;
 	var table = '<table class="table table-bordered">';
 	var thead = '<thead><tr><th>排名</th><th>博主ID</th><th>博主昵称</th><th>博主地域</th><th>博主微博</th><th>转发数</th><th>评论数</th><th>敏感状态</th></tr></thead>';
 	var tbody = '<tbody>';
-	for(var i = 0;i < rowCount;i++) {
+	for (var i = start_row;i < end_row;i++) {
             var tr = '<tr>';
 	    if (data[i][3].match("海外")) {
 		tr = '<tr class="success">';

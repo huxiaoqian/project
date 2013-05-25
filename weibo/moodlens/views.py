add8af4d11f9d7d1eecc54380417f393426b3a9b
MOON_CLJ <lijunli2598@gmail.com>
2013-05-25 11:32:11 +0800

    情绪接口切换成天

--- weibo/moodlens/views.py
+++ weibo/moodlens/views.py
@@ -25,6 +25,7 @@ def get_bucket(bucket):
 
 emotions_kv = {'happy': 1, 'angry': 2, 'sad': 3}
 s = XapianSearch(path='/opt/xapian_weibo/data/', name='master_timeline_sentiment', schema=Schema, schema_version=1)
+xapian_search_weibo = XapianSearch(path='/opt/xapian_weibo/data/', name='master_timeline_weibo')
 total_days = 90
 
 
@@ -33,67 +34,67 @@ def index():
     return render_template('moodlens/index.html', active='moodlens')
 
 
-@mod.route('/data/<emotion>/')
+@mod.route('/field')
+def field():
+    return render_template('moodlens/field_emotion.html', active='moodlens')
+
+
+@mod.route('/topic')
+def topic():
+    return render_template('moodlens/topic_emotion.html', active='moodlens')
+
 @mod.route('/data/<emotion>/<area>/')
 def data(emotion, area='global'):
     query = request.args.get('query', '')
     query = query.strip()
     ts = request.args.get('ts', '')
-    #ts = long(ts)
+    ts = long(ts)
 
-    today = datetime.datetime.today()
-    now_ts = time.mktime(datetime.datetime(today.year, today.month, today.day, 2, 0).timetuple())
-    now_ts = int(now_ts)
     during = 24 * 3600
 
-    data = []
     if query:
-        today = datetime.datetime.today()
-        now_ts = time.mktime(datetime.datetime(today.year, today.month, today.day, 2, 0).timetuple())
-        now_ts = int(now_ts)
-        during = 24 * 3600
+        begin_ts = ts - during
+        end_ts = ts
+        print begin_ts, end_ts
+        query_dict = {
+            'timestamp': {'$gt': begin_ts, '$lt': end_ts},
+            'sentiment': emotions_kv[emotion],
+            'text': query,
+        }
+        count, _ = s.search(query=query_dict)
 
-        for i in xrange(-total_days + 1, 1):
-            begin_ts = now_ts + during * (i - 1)
-            end_ts = now_ts + during * i
-            print i, begin_ts, end_ts
-            query_dict = {
-                'timestamp': {'$gt': begin_ts, '$lt': end_ts},
-                'sentiment': emotions_kv[emotion],
-                'text': query,
-            }
-            count, _ = s.search(query=query_dict)
-
-            data.append([end_ts * 1000, count])
+        data = [end_ts * 1000, count]
     else:
         bucket = get_bucket('weibo_daily_sentiment_count_%s' % area)
-        for i in xrange(-total_days + 1, 1):
-            end_ts = now_ts + during * i
-            try:
-                daily_emotion_count = bucket.Get(str(end_ts) + '_' + str(emotions_kv[emotion]))
-                daily_emotion_count = int(daily_emotion_count)
-            except KeyError:
-                daily_emotion_count = 0
-            data.append([end_ts * 1000, daily_emotion_count])
+        end_ts = ts
+        try:
+            daily_emotion_count = bucket.Get(str(end_ts) + '_' + str(emotions_kv[emotion]))
+            daily_emotion_count = int(daily_emotion_count)
+        except KeyError:
+            daily_emotion_count = 0
+        data = [end_ts * 1000, daily_emotion_count]
 
     print data
     return json.dumps(data)
 
 
-@mod.route('/flag_data/<emotion>/')
-def flag_data(emotion):
-    data = []
+@mod.route('/flag_data/<emotion>/<area>/')
+def flag_data(emotion, area='global'):
+    ts = request.args.get('ts', '')
+    ts = long(ts)
 
     today = datetime.datetime.today()
     now_ts = time.mktime(datetime.datetime(today.year, today.month, today.day, 2, 0).timetuple())
-    now_ts = int(now_ts)
+    now_ts = long(now_ts)
     during = 24 * 3600
 
-    iter_count = 0
+    timestamps = []
     for i in xrange(-total_days + 5, 1, 10):
-        iter_count += 1
-        begin_ts = now_ts + during * (i - 1)
-        end_ts = now_ts + during * i
+        timestamps.append(now_ts + during * i)
+
+    data = []
+    if ts in timestamps:
+        end_ts = ts
         query_dict = {
             'timestamp': {'$gt': end_ts - 3600, '$lt': end_ts},
             'sentiment': emotions_kv[emotion],
@@ -104,8 +105,50 @@ def flag_data(emotion):
         text = ','.join([tp[0] for tp in keywords_with_count])
         data.append({
             'x': end_ts * 1000,
-            'title': chr(ord('A') + iter_count),
+            'title': chr(ord('A') + timestamps.index(end_ts)),
             'text': text
         })
+
+    return json.dumps(data)
+
+
+@mod.route('/keywords_data/<emotion>/<area>/')
+def keywords_data(emotion, area='global'):
+    ts = request.args.get('ts', '')
+    ts = long(ts)
+
+    end_ts = ts
+    query_dict = {
+        'timestamp': {'$gt': end_ts - 3600, '$lt': end_ts},
+        'sentiment': emotions_kv[emotion],
+    }
+    count, get_results = s.search(query=query_dict, fields=['terms'])
+    print count
+    keywords_with_count = top_keywords(get_results, top=20)
+    print keywords_with_count
+    keywords_with_count = [list(i) for i in keywords_with_count]
+
+    return json.dumps(keywords_with_count)
+
+
+@mod.route('/weibos_data/<area>/')
+def weibos_data(area='global'):
+    query = request.args.get('query', '')
+    query = query.strip()
+
+    ts = request.args.get('ts', '')
+    ts = long(ts)
+
+    end_ts = ts
+    query_dict = {
+        'timestamp': {'$gt': end_ts - 3600, '$lt': end_ts},
+        '$or': [],
+    }
+    for term in query.split(','):
+        if term:
+            query_dict['$or'].append({'text': term})
+    count, get_results = xapian_search_weibo.search(query=query_dict, max_offset=5, sort_by=['reposts_count'], fields=['text', 'timestamp'])
+    print count
+    data = list(get_results())
 
     return json.dumps(data)

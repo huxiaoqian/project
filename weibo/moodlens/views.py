796e0e4988ecaea57d6188207ee3520c9f339fad
ystone1025 <ystone1025@163.com>
2013-12-18 17:46:54 +0800

    mc

--- weibo/moodlens/views.py
+++ weibo/moodlens/views.py
@@ -8,18 +8,19 @@ from weibo.global_config import xapian_search_weibo, emotions_kv, \
 from flask import Blueprint, render_template, request, session, redirect
 from utils import getWeiboByMid, st_variation, find_topN, read_range_kcount_results, \
                   sentimentFromDB, sentimentRealTime, read_range_weibos_results
-#from keywords import search_keywords, search_topic_keywords, search_domain_keywords
-import keywords as keywordsModule
 from xapian_weibo.utils import top_keywords
 from topics import _all_topics, _add_topic, _drop_topic
 import simplejson as json
-import datetime
+from datetime import date
+from datetime import datetime
 import time
 import leveldb
 import os
 import weibo.model
 import json
 import re
+
+month_value = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
 
 mod = Blueprint('moodlens', __name__, url_prefix='/moodlens')
 
@@ -33,6 +34,56 @@ def get_bucket(bucket):
     buckets[bucket] = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'lijun_' + bucket),
                                       block_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
     return buckets[bucket]
+
+def _utf_encode(s):
+    if isinstance(s, str):
+        return s
+    else:
+        return s.encode('utf-8')
+
+
+def _utf_decode(s):
+    if isinstance(s, str):
+        return s.decode('utf-8')
+    else:
+        return s
+
+def strToDate(dur_time):
+    m = '1'
+    d = '1'
+    y = '2013'
+    items = dur_time.split(', ')
+    n = 0
+    for item in items:
+        if n == 0:
+            mds = item.split(' ')
+            t = 0
+            for md in mds:
+                if t==0:
+                    m = month_value[md]
+                    t = 1
+                else:
+                    d = md
+            n = 1
+        else:
+            y = item
+
+    time_str = str(y)+'-'+str(m)+'-'+str(d)
+
+    return time_str
+
+def str2ts(s):
+    temp_during = _utf_encode(s)
+    if re.match(r'\d+分钟', temp_during):
+        pattern=re.compile(r'分钟')
+        temp_during=int(pattern.split(temp_during)[0])*60
+    elif re.match((r'\d+小时'),temp_during):
+        pattern=re.compile(r'小时')
+        temp_during=int(pattern.split(temp_during)[0])*3600
+    elif re.match(r'\d+天',temp_during):
+        pattern=re.compile(r'天')
+        temp_during=int(pattern.split(temp_during)[0])*24*3600
+    return temp_during
 
 @mod.route('/log_in', methods=['GET','POST'])
 def log_in():
@@ -62,8 +113,8 @@ def index():
         return redirect('/')
 
 
-@mod.route('/all', methods=['GET','POST'])
-def topic():
+@mod.route('/all/', methods=['GET','POST'])
+def all_emotion():
     if 'logged_in' in session and session['logged_in']:        
         if session['user'] == 'admin':
             dur_time = request.args.get('time', '')
@@ -151,7 +202,7 @@ def topic():
         return redirect('/')
 
 
-@mod.route('/field', methods=['GET','POST'])
+@mod.route('/field/', methods=['GET','POST'])
 def field():
     if 'logged_in' in session and session['logged_in']:        
         if session['user'] == 'admin':
@@ -169,34 +220,7 @@ def field():
     else:
         return redirect('/')
 
-def _utf_encode(s):
-    if isinstance(s, str):
-        return s
-    else:
-        return s.encode('utf-8')
-
-
-def _utf_decode(s):
-    if isinstance(s, str):
-        return s.decode('utf-8')
-    else:
-        return s
-
-
-def str2ts(s):
-    temp_during = _utf_encode(s)
-    if re.match(r'\d+分钟', temp_during):
-        pattern=re.compile(r'分钟')
-        temp_during=int(pattern.split(temp_during)[0])*60
-    elif re.match((r'\d+小时'),temp_during):
-        pattern=re.compile(r'小时')
-        temp_during=int(pattern.split(temp_during)[0])*3600
-    elif re.match(r'\d+天',temp_during):
-        pattern=re.compile(r'天')
-        temp_during=pattern.split(temp_during)*24*3600
-    return temp_during
-
-@mod.route('/topic', methods=['GET','POST'])
+@mod.route('/topic/', methods=['GET','POST'])
 def topic():
     if 'logged_in' in session and session['logged_in']:        
         if session['user'] == 'admin':
@@ -238,6 +262,7 @@ def data(area='global'):
     during = int(during)
     
     results = []
+    print 'here'
 
     if query and query != '':
         results = sentimentFromDB(end_ts, during, method='topic', query=query)
@@ -323,47 +348,63 @@ def flag_data(emotion, area='global'):
 
 @mod.route('/keywords_data/<area>/')
 def keywords_data(area='global'):
-    """情绪关键词数据
+    """
+    /keywords_data 接口已备好，只是差领域数据
     """
 
-    query = request.args.get('query', None)
-    if query:
-        query = query.strip()
-    during = request.args.get('during', 24*3600)
-    during = int(during)
-    print during
+    query = request.args.get('query', '')
+    query = query.strip()
     ts = request.args.get('ts', '')
     ts = long(ts)
+    during = request.args.get('during', 24*3600)
+    during = int(during)
+    
     begin_ts = ts - during
     end_ts = ts
-    limit = request.args.get('limit', 50)
-    limit = int(limit)
-    emotion = request.args.get('emotion', 'global')
+    results = read_range_kcount_results(begin_ts, end_ts, during)
 
-    results = {}
-
-    if area == 'global':
-        search_method = 'global'
-        if query:
-            search_method = 'topic'
-        area = None
-    else:
-        search_method = 'domain'
-        
-    search_func = getattr(keywordsModule, 'search_%s_keywords' % search_method, None)
-    print search_func
-
-    if search_func:
-        if emotion == 'global':
-            for k, v in emotions_kv.iteritems():
-                results[k] = search_func(end_ts, during, v, query=query, domain=area, top=limit)
-        else:
-            results[emotion] = search_func(end_ts, during, emotions_kv[emotion], query=query, domain=area, top=limit)
+    happy = results['happy']
+    sad = results['sad']
+    angry = results['angry']
+    happy = happy[:10]
+    sad = sad[:10]
+    angry = angry[:10]
+    results = {'happy':happy,'sad':sad,'angry':angry}
     
-    else:
-        return json.dumps('search function undefined')
-
     return json.dumps(results)
+
+
+@mod.route('/field_keywords_data/<area>/')
+def field_keywords_data(area):
+    """
+    /keywords_data 接口已备好，只是差领域数据
+    """
+    ts = request.args.get('ts', '')
+    ts = long(ts)
+    emotion = request.args.get('emotion', 'all')
+    during = request.args.get('during', 24*3600)
+    during = int(during)
+
+    begin_ts = ts - during
+    end_ts = ts
+    
+    query_dict = {
+        'timestamp': {'$gt': begin_ts, '$lt': end_ts},
+    }
+    if emotion != 'all':
+        query_dict['sentiment'] = emotions_kv[emotion]
+
+    count, field_users = xapian_search_domain.search(query={'domain':str(area)}, sort_by=['-followers_count'], fields=['_id'], max_offset=10000)
+    if count:
+        query_dict['$or'] = []
+        for user in field_users():
+            query_dict['$or'].append({'user': user['_id']})
+
+    count, get_results = xapian_search_weibo.search(query=query_dict, max_offset=100000, sort_by=['-reposts_count'], fields=['terms'])
+    keywords_with_count = top_keywords(get_results, top=50)
+    keywords_with_count = [list(i) for i in keywords_with_count]
+
+    return json.dumps(keywords_with_count)
 
 
 @mod.route('/weibos_data/<emotion>/<area>/')
@@ -518,3 +559,4 @@ def topics_customized():
             status, item = 'NoTopic', 'Null'
 
         return json.dumps({'status': status, 'item': item})
+

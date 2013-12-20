f0d68da35b661600381926965ded2644e80edebe
LinHaoBuaa <1257819385@qq.com>
2013-12-20 22:21:14 +0800

    lf

--- weibo/moodlens/peak_detection.py
+++ weibo/moodlens/peak_detection.py
@@ -1,107 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 import numpy as np
-def min_variation(lis,dur=7,form=0):
-	vs = []
-	for cursor in range(len(lis)):
-		if dur < len(lis):                       				
-			begin = cursor - int(dur/2)
-			if begin < 0:
-				begin = 0
-			end = begin + dur		
-			if end > len(lis)-1:
-	                    end = len(lis)-1
-	                    begin = end-dur
-		else:            
-			begin = 0
-			end = len(lis)-1
-		seg = lis[begin:end+1]
-		min_num = min(seg)
-		if form == 0:
-			vs.append((lis[cursor]-min_num))
-		else:
-			vs.append((lis[cursor]-min_num)/min_num)
-	return vs
 
-def filter_min_gap(lis,cursor,dur=7,form=1):
-	vs = min_variation(lis,dur=dur,form=form)
-	if vs[cursor] >= np.mean(vs):
-		return 1
-	else:
-		return 0
-
-def sentiment_variation(lis,cursor,dur=7):
-	stds = []
-	for i in range(len(lis)):
-		if i+dur > len(lis):
-			break
-		seg = lis[i:i+dur]
-		stds.append(np.std(seg))
-	global_std = np.mean(stds)
-	
-	if dur < len(lis):                       				
-		begin = cursor - int(dur/2)
+def scope(lens,cursor,dur):
+	##define the begin and end point of the scope which has #dur spots before and after the cursor
+	begin = 0
+	end = lens-1
+	if dur < lens:                       				
+		begin = cursor-dur
 		if begin < 0:
 			begin = 0
 		end = begin + dur		
-		if end > len(lis)-1:
-                    end = len(lis)-1
+		if end > lens-1:
+                    end = lens-1
                     begin = end-dur
-	else:            
-		begin = 0
-		end = len(lis)-1
-	
-	seg = lis[begin:end]
+	return begin,end
 
-	ave = (seg[0]+seg[-1])/2
-	local_std = np.std(seg)
-	# print begin,end,cursor,seg,ave,ave*1.2,lis[cursor],global_std,local_std,'seg begin,end,cursor'+st	
-	if lis[cursor]> 1.2*ave or local_std > 0.5*global_std:
-		return 1
-	else:
-		return 0
-
-def save_domain(lis,cursor,dur=11):
-	if dur < len(lis):                       				
-		begin = cursor - int(dur/2)
-		st = ''
-		if begin < 0:
-			begin = 0
-		end = begin + dur		
-		if end > len(lis)-1:
-                    end = len(lis)-1
-                    begin = end-dur
-	else:            
-		begin = 0
-		end = len(lis)-1
-
-	seg = lis[begin:end+1]
-	if lis[cursor] == max(seg):
-		return 1
-	else:
-		return 0
-
-def ema_list(lis,day):
-	day = day+1
-	ema = []
-	for i in range(1,len(lis)):
-		ema.append(lis[i-1]*(day-2)/day+lis[i]*2/day)
-	ema.append(lis[-2]*(day-2)/day+lis[-1]*2/day)
-	return ema
-
-def dif_list(lis_s,lis_l):
-	return [lis_s[i]-lis_l[i] for i in range(len(lis_s))]
-
-def dea_lis(dif,day):
-	day = day+1
-	deas = [dif[0]]
-	for i in range(1,len(dif)):
-		deas.append(deas[i-1]*(day-2)/day+dif[i]*2/day)
-	return deas
-def MACD_list(dif,dea_lis):
-	return [dif[i]-dea_lis[i] for i in range(len(dif))]
-
-def find_topN(lis,n):
+def sort_list(lis):
 	new = [lis[0]]
 	rank = [0]
 	num_cursor = 1
@@ -123,48 +38,112 @@ def find_topN(lis,n):
 			if find == 0:
 				new.append(num)
 				rank.append(num_cursor-1)
-			
+	return new,rank
+
+def filter_continuous(peaks):
+	## filter continues nodes(e.g lis[0] lis[1]), lis[1] will be filtered
+	new_peaks = []
+	delt = []
+
+	for pk in peaks:
+		if new_peaks == []:
+			new_peaks.append(pk)
+		else:
+			if pk-1 in new_peaks:
+				delt.append(pk)
+			new_peaks.append(pk)
+	print 'filter_continuous',delt
+	new_peaks = [pk for pk in new_peaks if pk not in delt]
+	return new_peaks
+
+def min_variation(lis,dur=3,form=0):
+	vs = []
+	for cursor in range(len(lis)):
+		begin,end = scope(len(lis),cursor,dur=dur)
+		seg = lis[begin:end+1]
+		min_num = min(seg)
+		if form == 0:
+			vs.append((lis[cursor]-min_num))
+		else:
+			vs.append((lis[cursor]-min_num)/min_num)
+	return vs
+
+def filter_min_gap(lis,cursor,dur=3,form=0):
+	vs = min_variation(lis,dur=dur,form=form)
+	if vs[cursor] >= np.mean(vs):
+		return 1
+	else:
+		return 0
+
+def sentiment_variation(lis,cursor,dur=3):
+	stds = []
+	for i in range(len(lis)):
+		if i+dur > len(lis):
+			break
+		seg = lis[i:i+dur]
+		stds.append(np.std(seg))
+	global_std = np.mean(stds)
+	
+	begin,end = scope(len(lis),cursor,dur=dur)	
+	seg = lis[begin:end]
+	ave = (seg[0]+seg[-1])/2
+	local_std = np.std(seg)
+	if lis[cursor]> 1.2*ave or local_std > 0.5*global_std:
+		return 1
+	else:
+		return 0
+
+def filter_flat(lis,peaks):
+	filters = []
+	for pk in peaks:
+		if pk < 0 or pk > len(lis)-1:
+			filters.append(pk)
+			continue
+		vr = sentiment_variation(lis,pk)
+		if vr == 0:
+			filters.append(pk)
+	peaks = [pk for pk in peaks if pk not in filters]
+	return peaks,filters
+
+def save_macro(lis,cursor,dur=6):
+	begin,end = scope(len(lis),cursor,dur=dur)
+	seg = lis[begin:end+1]
+	if lis[cursor] == max(seg):
+		return 1
+	else:
+		return 0
+
+
+def find_topN(lis,topN):
+	new,rank = sort_list(lis)	
 	peak_x = []
-	peak_y = []
 	cursor = 0
 	for y in new:
 		if rank[cursor]!=0 and rank[cursor]!=len(new)-1:
 			if y >= lis[rank[cursor]+1] and y >= lis[rank[cursor]-1]:
 				peak_x.append(rank[cursor])
-
-		elif rank[cursor]==0:
-			if y >= lis[rank[cursor]+1]:
-				peak_x.append(rank[cursor])
-
-		elif rank[cursor]==len(new)-1:
-			if y >= lis[rank[cursor]-1]:
-				peak_x.append(rank[cursor])
-
-		if len(peak_x)==n:
-			break
 		cursor += 1
-	return peak_x[:n]
+	peaks = filter_continuous(peak_x)
+	return peaks[:topN]
 
-def filter_variation(lis,peaks):
-	cursor = -1
-	filters = []
-	cursors = []
-	for zr in peaks:
-		cursor += 1
-		if zr < 0 or zr > len(lis)-1:
-			filters.append(zr)
-			cursors.append(cursors)
+def filter_micro_macro(lis,peaks,micro_dur=3,macro_dur=6,form=0):
+	vs = min_variation(lis,dur=micro_dur,form=form)
+	peak_gap,peak_rank = sort_list([vs[pk] for pk in peaks])
+	
+	delts = []
+	for pk in peak_rank:
+		if pk in delts:
 			continue
-		vr = sentiment_variation(lis,zr)
-		if vr == 0:
-			filters.append(zr)
-			cursors.append(cursors)
-	peaks = [peaks[i] for i in range(len(peaks)) if i not in cursors]
+		begin,end = scope(len(lis),pk,dur=macro_dur)
+		for i in range(peak_rank.index(pk)+1,len(peak_rank)):
+			if peak_rank[i] >= begin and peak_rank[i] <= end:
+				if lis[peak_rank[i]] < lis[pk]:
+					delts.append(peak_rank[i])
+	print'miciro_macro filter nodes:', delts
+	return [pk for pk in peaks if pk not in delts]
 
-	return peaks,filters
 
-
-def detect_peaks(lis,topN=10,form=1):
+def detect_peaks(lis,topN=10,form=0,micro_dur=5,macro_dur=10):
 	if len(lis) ==[]:
 		return []
 	elif len(lis) == 1:
@@ -176,23 +155,21 @@ def detect_peaks(lis,topN=10,form=1):
 			peaks.append(0)
 		if lis[-1] > lis[-2]:
 			peaks.append(len(lis)-1)
-		print peaks,'step2 edge nodes'
-	# peaks,filters = filter_variation(lis,peaks)
-	# print filters,'filtered nodes'
-	# print peaks,'step3 MACD nodes'
 	
 	remove_nodes = []
+	scan_nodes = []
 	for pk in peaks:
 		remove_stay = [0,0]
-		remove_stay[0] = filter_min_gap(lis,pk,dur=7,form=1)
-		remove_stay[1] = save_domain(lis,pk,dur=13)
-		print pk,remove_stay
-		
+		remove_stay[0] = filter_min_gap(lis,pk,dur=micro_dur,form=0)
+		remove_stay[1] = save_macro(lis,pk,dur=macro_dur)
+		scan_nodes.append([pk,remove_stay])		
 		if remove_stay == [0,0]:
 			remove_nodes.append(pk)
-	print 'remove_nodes',remove_nodes
+	print scan_nodes,'remove_nodes',remove_nodes
+
 	new_zeros = set([pk for pk in peaks if pk not in remove_nodes])
 	new_zeros = list(new_zeros)
+	new_zeros = filter_micro_macro(lis,new_zeros,micro_dur=3,macro_dur=6,form=0)
 	new_zeros = sorted(new_zeros)	
 	print new_zeros,'final nodes'
 	return new_zeros

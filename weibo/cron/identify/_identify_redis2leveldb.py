bf626b8952bc6fc231e55bebc7aab665c8c090fd
LinHaoBuaa <1257819385@qq.com>
2014-01-05 23:47:05 +0800

    little fix

--- weibo/cron/identify/_identify_redis2leveldb.py
+++ weibo/cron/identify/_identify_redis2leveldb.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import os
 import redis
-import heapq
+import time
+import leveldb
 import datetime
-from model import WholeIdentification
-from time_utils import datetimestr2ts, ts2datetime
-from config import REDIS_HOST, REDIS_PORT, LEVELDBPATH, fields_value, labels, db
+from config import REDIS_HOST, REDIS_PORT, LEVELDBPATH, db
 
-
-CAL_TOPK = 1000
-SAVE_TOPK = 1000
-DOMAINS_LIST = fields_value + labels
 USER_DOMAIN = "user_domain" # user domain hash,
 GLOBAL_ACTIVE_COUNT = "global_active_%s" # date as '20131227',
 GLOBAL_IMPORTANT_COUNT = "global_important_%s" # date as '20131227',
-DOMAIN_ACTIVE_COUNT = "domain_active_%s:%s" # date as '20131227', domain
-DOMAIN_IMPORTANT_COUNT = "domain_important_%s:%s" # date as '20131227', domain
-user_followers_bucket = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'yuanshi_daily_user_followers'),
+user_followers_bucket = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'linhao_daily_user_followers'),
                                         block_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
 
 
 def get_daily_user_count_db_by_date(datestr):
     # datestr '20140105'
     daily_user_count_db = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'yuanshi_daily_count_%s' % datestr),
-                                          lock_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
+                                          block_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
     return daily_user_count_db
 
 
@@ -80,6 +74,8 @@ def redis2leveldb():
             key = str(uid)
             value = str(active) + '_' + str(important) + '_' + str(followers_count) + '_' + str(domain)
             global_leveldb.Put(key, value)
+            global_r0.hdel(GLOBAL_ACTIVE_COUNT % now_datestr, uid)
+            global_r0.hdel(GLOBAL_IMPORTANT_COUNT % now_datestr, uid)
 
         next_cursor, result_dicts = global_r0.hscan(GLOBAL_ACTIVE_COUNT % now_datestr)
 
@@ -88,6 +84,11 @@ def redis2leveldb():
             te = time.time()
             print count, '%s sec' % (te - ts)
             ts = te
+
+
+def test_read():
+    for k, v in global_leveldb.RangeIter():
+        print k, v
         
 
 if __name__ == '__main__':
@@ -96,3 +97,4 @@ if __name__ == '__main__':
     global_leveldb = get_daily_user_count_db_by_date(now_datestr)
 
     redis2leveldb()
+    #test_read()

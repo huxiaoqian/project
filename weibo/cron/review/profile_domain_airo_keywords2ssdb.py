f298c9025643b2be5235063c64929055c5f0203f
linhaobuaa <1257819385@qq.com>
2014-03-22 12:22:29 +0800

    little fix

--- weibo/cron/review/profile_domain_airo_keywords2ssdb.py
+++ weibo/cron/review/profile_domain_airo_keywords2ssdb.py
@@ -6,17 +6,22 @@ import leveldb
 from SSDB import SSDB
 from config import SSDB_HOST, SSDB_PORT, LEVELDBPATH
 
+DOMAIN_TOPK_KEYWORDS_SSDB_HASH = 'domain_topk_keywords_hash:%s' # datestr as '20130901'
+DOMAIN_AIRO_SSDB_HASH = 'domain_airo_hash:%s' # datestr as '20130901'
+
 
 def profile_domain_keywords2ssdb():
-    for i in range(9, 21):
-        keywords_bucket = daily_profile_domain_topk_keywords_bucket[i]
-	for k, v in keywords_bucket.RangeIter():
-	    print k, v
+	  for i in range(9, 21):
+	  	  keywords_bucket = daily_profile_domain_topk_keywords_bucket[i]
+	  	  for k, v in keywords_bucket.RangeIter():
+	  	   	  print k, v
+	  	   	  print now_datestr, ' saved ', ssdb.request('hset', [DOMAIN_TOPK_KEYWORDS_SSDB_HASH % now_datestr, str(i), v])
 
 
 def profile_domain_airo2ssdb():
     for k, v in daily_profile_domain_airo_bucket.RangeIter():
         print k, v
+        print now_datestr, ' saved ', ssdb.request('hset', [DOMAIN_AIRO_SSDB_HASH % now_datestr, k, v])
 
 
 if __name__ == '__main__':

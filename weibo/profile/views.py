c93c28be7a88171ef1c7666d499c85fe97ddbf66
linhaobuaa <1257819385@qq.com>
2014-03-23 16:45:33 +0800

    little fix

--- weibo/profile/views.py
+++ weibo/profile/views.py
@@ -26,7 +26,7 @@ from utils import getUserNameById, getUserIdByName, merge, \
                   yymInfo, _utf_8_decode, getUserInfoById, _utf_8_encode
 from time_utils import ts2HMS, last_week_to_date, ts2date, datetimestr2ts, ts2datetime, ts2datetimestr
 from weibo.global_config import xapian_search_user, LEVELDBPATH, \
-                                fields_value, fields_id, emotions_zh_kv, emotions_kv, LATEST_DATE, DOMAIN_LIST, \
+                                emotions_zh_kv, emotions_kv, LATEST_DATE, DOMAIN_LIST, \
                                 DOMAIN_ZH_LIST
 
 from _multi_search import _hotest_users, _newest_users, _domain_users
@@ -34,17 +34,34 @@ from _mysql import _search_person_basic, _search_person_important_active, _multi
 from _ssdb import getPersonData, getDomainKeywordsData, getDomainCountData, getDomainBasic
 
 
-buckets = {}
 mod = Blueprint('profile', __name__, url_prefix='/profile')
-COUNT_PER_PAGE = 20
-month_value = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
-fields_value = ['culture', 'education', 'entertainment', 'fashion', 'finance', 'media', 'sports', 'technology', 'oversea']
-labels = ['university', 'homeadmin', 'abroadadmin', 'homemedia', 'abroadmedia', 'folkorg', \
-          'lawyer', 'politician', 'mediaworker', 'activer', 'grassroot', 'other']
 
-#DOMAIN_LIST = fields_value + labels
-DOMAIN_LIST = DOMAIN_LIST[9:]
-DOMAIN_ZH_LIST = DOMAIN_ZH_LIST[9:]
+COUNT_PER_PAGE = 20
+
+
+def get_default_timerange():
+    return u'9月 15日,2013 - 9月 21日,2013'
+
+
+def get_default_field_dict():
+    field_dict = {}
+    for idx, field_en in enumerate(DOMAIN_LIST[9:20]):
+        field_dict[field_en] = DOMAIN_ZH_LIST[idx+9]
+
+    return field_dict
+
+
+def get_default_field_name():
+    return 'activer', u'活跃人士'
+
+
+def get_default_hotest_time():
+    return ts2datetime(datetimestr2ts(LATEST_DATE) - 24 * 3600)
+
+
+default_timerange = get_default_timerange()
+default_field_dict = get_default_field_dict()
+default_field_enname, default_field_zhname = get_default_field_name()
 
 
 def _time_zone(stri):
@@ -70,50 +87,6 @@ def _time_zone(stri):
     return int(start_ts), int(end_ts)
 
 
-def fieldsEn2Zh(name):
-    if name == 'finance':
-        return u'财经'
-    if name == 'media':
-        return u'媒体'
-    if name == 'culture':
-        return u'文化'
-    if name == 'technology':
-        return u'科技'
-    if name == 'entertainment':
-        return u'娱乐'
-    if name == 'education':
-        return u'教育'
-    if name == 'fashion':
-        return u'时尚'
-    if name == 'sports':
-        return u'体育'
-    if name == 'oversea':
-        return u'境外'
-    if name == 'university':
-        return u'高校微博'
-    if name == 'homeadmin':
-        return u'境内机构'
-    if name == 'abroadadmin':
-        return u'境外机构'
-    if name == 'homemedia':
-        return u'境内媒体'
-    if name == 'abroadmedia':
-        return u'境外媒体'
-    if name == 'folkorg':
-        return u'民间组织'
-    if name == 'lawyer':
-        return u'律师'
-    if name == 'politician':
-        return u'政府官员'
-    if name == 'mediaworker':
-        return u'媒体人士'
-    if name == 'activer':
-        return u'活跃人士'
-    if name == 'grassroot':
-        return u'草根'
-    if name == 'other':
-        return u'其它'
-
 def getStaticInfo():
     statuscount = [0, 2000000, 4000000, 6000000, 8000000, 10000000, 12000000, 14000000, 16000000, 18000000, 20000000]
     friendscount = [0, 400, 800, 1200, 1600, 2000, 2400, 2800, 3200, 3600, 4000]
@@ -123,8 +96,8 @@ def getStaticInfo():
     friendsRange = [{'lowBound': friendscount[i], 'upBound': friendscount[i+1]} for i in range(len(friendscount)-1)]
     followersRange = [{'lowBound': followerscount[i], 'upBound': followerscount[i+1]} for i in range(len(followerscount)-1)]
     province = [{'province': unicode(i, 'utf-8')} for i in province]
-    fields = [{'fieldEnName': f, 'fieldZhName': DOMAIN_ZH_LIST[i]} for i, f in enumerate(DOMAIN_LIST)]
-    return statusRange, friendsRange, followersRange, province, fields
+
+    return statusRange, friendsRange, followersRange, province
 
 @mod.route('/log_in', methods=['GET','POST'])
 def log_in():
@@ -138,16 +111,18 @@ def log_in():
 @mod.route('/', methods=['GET','POST'])
 def index():
     if 'logged_in' in session and session['logged_in']:
-        statuscount, friendscount, followerscount, province, field = getStaticInfo()
+        statuscount, friendscount, followerscount, province = getStaticInfo()
         if session['user'] == 'admin':
-            return render_template('profile/index.html', fields=field)
+            return render_template('profile/index.html', timerange=default_timerange, field_dict=default_field_dict, \
+                                   field_zh=default_field_zhname, field_en=default_field_enname)
         else:
             pas = db.session.query(UserList).filter(UserList.username==session['user']).all()
             if pas != []:
                 for pa in pas:
                     identy = pa.identify
                     if identy == 1:
-                        return render_template('profile/index.html', fields=field)
+                        return render_template('profile/index.html', timerange=default_timerange, field_dict=default_field_dict, \
+                                   field_zh=default_field_zhname, field_en=default_field_enname)
                     else:
                         return redirect('/')
             return redirect('/')
@@ -160,16 +135,17 @@ def profile_search(model='hotest'):
     if 'logged_in' in session and session['logged_in']:
         if session['user'] == 'admin':
             if request.method == 'GET':
-                statuscount, friendscount, followerscount, province, field = getStaticInfo()
+                statuscount, friendscount, followerscount, province = getStaticInfo()
                 province_str = ''
                 province = ['北京',  '上海', '香港', '台湾', '重庆', '澳门', '天津', '江苏', '浙江', '四川', '江西', '福建', '青海', '吉林', '贵州', '陕西', '山西', '河北', '湖北', '辽宁', '湖南', '山东', '云南', '河南', '广东', '安徽', '甘肃', '海南', '黑龙江', '内蒙古', '新疆', '广西', '宁夏', '西藏', '海外'] 
                 for pro in province:
                     province_str += unicode(pro, 'utf-8') + ','
                 if model == 'person':
                     nickname = urllib2.unquote(request.args.get('nickname'))
-                    return render_template('profile/profile_search.html',statuscount=statuscount,
-                                           friendscount=friendscount, followerscount=followerscount,
-                                           location=province_str, field=field, model=model, result=None, nickname=nickname)
+                    return render_template('profile/profile_search.html',statuscount=statuscount, \
+                                           friendscount=friendscount, followerscount=followerscount, \
+                                           location=province_str, model=model, result=None, nickname=nickname, \
+                                           field_dict=default_field_dict)
                 elif model == 'find':
                     statuses_count_upBound = request.args.get('statuses_count_upBound',None)
                     friends_count_upBound = request.args.get('friends_count_upBound',None)
@@ -198,11 +174,11 @@ def profile_search(model='hotest'):
 
                     return render_template('profile/profile_search.html',result_count = result_count, statusescount_up = statusescount_up,
                                            friendscount_up = friendscount_up,followerscount_up = followerscount_up,
-                                           location = province_str, field=field, model = model,rankcount=rank_str)  
+                                           location = province_str, model = model,rankcount=rank_str, field_dict=default_field_dict)  
                 else:
                     return render_template('profile/profile_search.html',statuscount=statuscount,
                                            friendscount=friendscount, followerscount=followerscount,
-                                           location=province_str, field=field, model=model, result=None)
+                                           location=province_str, model=model, result=None, field_dict=default_field_dict)
             if request.method == 'POST' and request.form['page']:
                 if model == 'newest':
                     top_n = 1000
@@ -217,7 +193,7 @@ def profile_search(model='hotest'):
 
                 elif model == 'hotest':
                     top_n = 1000
-                    latest = ts2datetime(datetimestr2ts(LATEST_DATE))
+                    latest = get_default_hotest_time()
                     page = int(request.form['page'])
                     if page == 1:
                         startoffset = 0
@@ -327,7 +303,7 @@ def profile_search(model='hotest'):
 def profile_group():
     if 'logged_in' in session and session['logged_in']:
         if session['user'] == 'admin':
-            statuscount, friendscount, followerscount, province, field = getStaticInfo()
+            statuscount, friendscount, followerscount, province = getStaticInfo()
             if request.method == 'GET':
                 fieldEnName = request.args.get('fieldEnName', None)
                 during_time = request.args.get('during_time', None)
@@ -339,8 +315,8 @@ def profile_group():
                     end_ts = datetimestr2ts(datestr)
                     start_ts = end_ts - interval * 24 * 3600
 
-            return render_template('profile/profile_group.html', field=field, model=fieldEnName, \
-                                   atfield=DOMAIN_ZH_LIST[DOMAIN_LIST.index(fieldEnName)], \
+            return render_template('profile/profile_group.html', model=fieldEnName, \
+                                   atfield=default_field_dict[fieldEnName], field_dict=default_field_dict, \
                                    start_ts=start_ts, end_ts=end_ts)
         else:
             return redirect('/')
@@ -356,9 +332,8 @@ def profile_person(uid):
         if session['user'] == 'admin':
             if uid:
                 user = {}
-                datestr = '20130904'
-                interval = 7
-                date_list = [datestr]#last_week_to_date(datestr, interval)
+                datestr = LATEST_DATE
+                date_list = [datestr]
                 active = important = reposts = original = emoticon = 0
                 for current_time in date_list:
                     _active, _important, _reposts, _original, _emoticon, _direct_interact, _retweeted_interact, _keywords_dict = getPersonData(uid, current_time)
@@ -431,8 +406,22 @@ def profile_interact_network(uid):
         fri_fol.extend(friends)
         fri_fol.extend(followers)
 
-        datestr = '20130907'
-        interval = 7
+        start_ts = request.args.get('start_ts', None)
+        end_ts = request.args.get('end_ts', None)
+
+        if start_ts:
+            start_ts = int(start_ts)
+
+        if end_ts:
+            end_ts = int(end_ts)
+
+        try:
+            interval = (end_ts - start_ts) / (24 * 3600) + 1
+            datestr = ts2datetimestr(end_ts) # '20130907'
+        except Exception, e:
+            print e
+            interval, datestr = _default_time()
+
         date_list = last_week_to_date(datestr, interval)
     
         for datestr in date_list:
@@ -600,8 +589,22 @@ def profile_person_topic(uid):
         result_arr = []
         limit = 50
 
-        datestr = '20130907'
-        interval = 7
+        start_ts = request.args.get('start_ts', None)
+        end_ts = request.args.get('end_ts', None)
+
+        if start_ts:
+            start_ts = int(start_ts)
+
+        if end_ts:
+            end_ts = int(end_ts)
+        
+        try:
+            interval = (end_ts - start_ts) / (24 * 3600) + 1
+            datestr = ts2datetimestr(end_ts) # '20130907'
+        except Exception, e:
+            print e
+            interval, datestr = _default_time()
+
         date_list = last_week_to_date(datestr, interval)
         active = important = reposts = original = emoticon = 0
         keywords_dict = {}
@@ -630,7 +633,6 @@ def profile_person_topic(uid):
 def profile_network(friendship, uid):
     if request.method == 'GET':
         limit = 10
-        interval = 7
 
         fri_fol = getFriendship(uid, friendship)
 
@@ -649,7 +651,22 @@ def profile_network(friendship, uid):
 
         domain_count = [(k, v) for k, v in domain_count.iteritems()]
 
-        datestr = '20130907'
+        start_ts = request.args.get('start_ts', None)
+        end_ts = request.args.get('end_ts', None)
+
+        if start_ts:
+            start_ts = int(start_ts)
+
+        if end_ts:
+            end_ts = int(end_ts)
+        
+        try:
+            interval = (end_ts - start_ts) / (24 * 3600) + 1
+            datestr = ts2datetimestr(end_ts) # '20130907'
+        except Exception, e:
+            print e
+            interval, datestr = _default_time()
+
         date_list = last_week_to_date(datestr, interval)
     
         for datestr in date_list:
@@ -735,6 +752,7 @@ def personal_weibo_count(uid):
         post_arr.append(active)
         repost_arr.append(reposts)
         fipost_arr.append(original)
+        print important
         im_arr.append(important)
         emot_arr.append(emoticon)
         time_arr.append(datestr)
@@ -744,7 +762,7 @@ def personal_weibo_count(uid):
     emoticons_count = sum(emot_arr)
 
     if total_post_count != 0:
-        emoticons_ratio = int(emoticons_count * 100 / total_post_count) / 100.0
+        emoticons_ratio = int(emoticons_count * 100 / total_post_count) / 100.000
         retweetes_ratio = int(retweets_count * 100 / total_post_count) / 100.0
     else:
         emoticons_ratio = 0.0
@@ -779,7 +797,7 @@ def profile_group_topic(fieldEnName):
         except Exception, e:
             interval, datestr = _default_time()
 
-        domainid = DOMAIN_LIST.index(fieldEnName) + 9
+        domainid = DOMAIN_LIST.index(fieldEnName)
         date_list = last_week_to_date(datestr, interval)
 
         keywords_dict = {}
@@ -819,7 +837,7 @@ def profile_group_status_count(fieldEnName):
         interval, datestr = _default_time()
 
     date_list = last_week_to_date(datestr, interval)
-    domainid = DOMAIN_LIST.index(fieldEnName) + 9
+    domainid = DOMAIN_LIST.index(fieldEnName)
 
     time_arr = []
     total_arr = []
@@ -840,7 +858,7 @@ def profile_group_status_count(fieldEnName):
 
 def _default_time():
     interval = 7
-    datestr = '20130907'
+    datestr = LATEST_DATE
     return interval, datestr
 
 @mod.route('/group_important/<fieldEnName>', methods=['GET', 'POST'])
@@ -861,7 +879,7 @@ def group_active_count(fieldEnName):
         interval, datestr = _default_time()
 
     date_list = last_week_to_date(datestr, interval)
-    domainid = DOMAIN_LIST.index(fieldEnName) + 9
+    domainid = DOMAIN_LIST.index(fieldEnName)
 
     time_arr = []
     important_arr = []
@@ -878,7 +896,7 @@ def group_active_count(fieldEnName):
 @mod.route('/group_verify/<fieldEnName>')
 def profile_group_verify(fieldEnName):
     interval, datestr = _default_time()
-    domainid = DOMAIN_LIST.index(fieldEnName) + 9
+    domainid = DOMAIN_LIST.index(fieldEnName)
     date_list = last_week_to_date(datestr, interval)
     date_list.reverse()
 
@@ -901,7 +919,7 @@ def profile_group_verify(fieldEnName):
 @mod.route('/group_location/<fieldEnName>')
 def profile_group_location(fieldEnName):
     interval, datestr = _default_time()
-    domainid = DOMAIN_LIST.index(fieldEnName) + 9
+    domainid = DOMAIN_LIST.index(fieldEnName)
     date_list = last_week_to_date(datestr, interval)
     date_list.reverse()
 

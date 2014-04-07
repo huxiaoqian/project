45c84893024f6f799105fc94b2f476336a9e68b8
linhaobuaa <1257819385@qq.com>
2014-04-07 17:53:26 +0800

    little fix

--- weibo/graph/graph.py
+++ weibo/graph/graph.py
@@ -21,7 +21,7 @@ from flask import Blueprint, session, redirect, url_for
 from weibo.global_config import XAPIAN_RESTFUL_HOST, XAPIAN_RESTFUL_PORT
 
 weibo_fields = ['_id', 'user', 'retweeted_uid', 'retweeted_mid', 'text', 'timestamp', \
-                'reposts_count', 'source', 'bmiddle_pic', 'geo', 'attitudes_count', \
+                'reposts_count', 'bmiddle_pic', 'geo', 'attitudes_count', \
                 'comments_count', 'sentiment']
 user_fields = ['_id', 'province', 'city', 'verified', 'name', 'friends_count', \
                'gender', 'profile_image_url', 'verified_type', 'followers_count', \

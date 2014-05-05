3cdf576a0ead001cb262c8789e8be9364d49dfae
linhaobuaa <1257819385@qq.com>
2014-05-05 23:51:04 +0800

    little fix

--- test_hbase/test_batch_read_weibo.py
+++ test_hbase/test_batch_read_weibo.py
@@ -47,12 +47,82 @@ def test_iter_all_docs():
         count += 1
 
 
+def test_iter_all_docs_columns():
+    # 10 seconds per 10000 weibos
+    weibos = xapian_weibo.iter_all_docs(fields=['_id'])
+    count = 0
+    tb = time.time()
+    ts = tb    
+    for weibo in weibos:
+        mid = weibo['_id']
+        row = table.row(str(mid), columns=['o:bmiddle_pic', 'o:comments_count', 'o:geo', 'o:reposts_count', \
+                                           'o:retweeted_mid', 'o:retweeted_uid', 'o:sentiment', 'o:terms', \
+                                           'o:text', 'o:timestamp', 'o:user'])
+        if row != {}:
+            bmiddle_pic = row['o:bmiddle_pic']
+            comments_count = row['o:comments_count']
+            geo = row['o:geo']
+            reposts_count = row['o:reposts_count']
+            retweeted_mid = row['o:retweeted_mid']
+            retweeted_uid = row['o:retweeted_uid']
+            sentiment = row['o:sentiment']
+            terms = json.loads(row['o:terms'])
+            text = row['o:text']
+            timestamp = row['o:timestamp']
+            user = row['o:user']
+
+        if count % (XAPIAN_FLUSH_DB_SIZE * 10) == 0:
+            te = time.time()
+            print '[%s] search speed: %s sec/per %s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), te - ts, XAPIAN_FLUSH_DB_SIZE * 10)
+            if count % (XAPIAN_FLUSH_DB_SIZE * 100) == 0:
+                print '[%s] total search %s, cost: %s sec [avg: %sper/sec]' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), count, te - tb, count / (te - tb))
+            ts = te
+        count += 1
+
+
+def test_iter_all_docs_batch(batch_size=10000):
+    # 20 seconds per 10000 weibos
+    weibos = xapian_weibo.iter_all_docs(fields=['_id'])
+    count = 0
+    tb = time.time()
+    ts = tb
+    batch_mids = []
+    for weibo in weibos:
+        mid = weibo['_id']
+        batch_mids.append(str(mid))
+
+        if count % 10000 == 0:
+            rows = table.rows(batch_mids)
+            for key, row in rows:
+                if row != {}:
+                    bmiddle_pic = row['o:bmiddle_pic']
+                    comments_count = row['o:comments_count']
+                    geo = row['o:geo']
+                    reposts_count = row['o:reposts_count']
+                    retweeted_mid = row['o:retweeted_mid']
+                    retweeted_uid = row['o:retweeted_uid']
+                    sentiment = row['o:sentiment']
+                    terms = json.loads(row['o:terms'])
+                    text = row['o:text']
+                    timestamp = row['o:timestamp']
+                    user = row['o:user']
+            batch_mids = []
+
+        if count % (XAPIAN_FLUSH_DB_SIZE * 10) == 0:
+            te = time.time()
+            print '[%s] search speed: %s sec/per %s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), te - ts, XAPIAN_FLUSH_DB_SIZE * 10)
+            if count % (XAPIAN_FLUSH_DB_SIZE * 100) == 0:
+                print '[%s] total search %s, cost: %s sec [avg: %sper/sec]' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), count, te - tb, count / (te - tb))
+            ts = te
+        count += 1
+
 def test_search_by_topic(topic):
     count, get_results = xapian_weibo.search(query={'text': topic}, fields=['_id'])
     print count
     count = 0
     tb = time.time()
-    ts = tb 
+    ts = tb
+
     for r in get_results():
         mid = r['_id']
         row = table.row(str(mid))
@@ -78,7 +148,54 @@ def test_search_by_topic(topic):
         count += 1
 
 
+def test_search_by_topic_batch(topic, batch_size=10000):
+    count, get_results = xapian_weibo.search(query={'text': topic}, fields=['_id'])
+    print count
+    count = 0
+    tb = time.time()
+    ts = tb
+    batch_mids = []
+    for r in get_results():
+        mid = r['_id']
+        batch_mids.append(str(mid))
+
+        if count % 10000 == 0:
+            rows = table.rows(batch_mids)
+            for key, row in rows:
+                if row != {}:
+                    bmiddle_pic = row['o:bmiddle_pic']
+                    comments_count = row['o:comments_count']
+                    geo = row['o:geo']
+                    reposts_count = row['o:reposts_count']
+                    retweeted_mid = row['o:retweeted_mid']
+                    retweeted_uid = row['o:retweeted_uid']
+                    sentiment = row['o:sentiment']
+                    terms = json.loads(row['o:terms'])
+                    text = row['o:text']
+                    timestamp = row['o:timestamp']
+                    user = row['o:user']
+            batch_mids = []
+
+        if count % (XAPIAN_FLUSH_DB_SIZE * 10) == 0:
+            te = time.time()
+            print '[%s] search speed: %s sec/per %s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), te - ts, XAPIAN_FLUSH_DB_SIZE * 10)
+            if count % (XAPIAN_FLUSH_DB_SIZE * 100) == 0:
+                print '[%s] total search %s, cost: %s sec [avg: %sper/sec]' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), count, te - tb, count / (te - tb))
+            ts = te
+        count += 1
+
+
 if __name__ == '__main__':
     # test_iter_all_docs()
 
-    test_search_by_topic(u'中国')
+    # test_search_by_topic(u'中国')
+
+    # test_iter_all_docs_columns()
+
+    # test_iter_all_docs_batch()
+    
+    # 40 seconds per 10000 weibos
+    test_search_by_topic_batch(u'中国')
+    
+    # seconds per 10000 weibos
+    test_search_by_topic_batch(u'中国', 5000)
---
+++ test_hbase/test_multiprocessing_search.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import os
+import multiprocessing
+from xapian_weibo.xapian_backend import XapianSearch
+
+stub_files = ['/home/ubuntu12/dev/data/stub/master_timeline_weibo_20130901', \
+              '/home/ubuntu12/dev/data/stub/master_timeline_weibo_20130902']
+
+def test_search(stub_file):
+    xapian_weibo = XapianSearch(stub=stub_file, schema_version=5, multi_processing=False)
+    count, get_results = xapian_weibo.search(query={'text': u'中国'}, fields=['text', 'terms'])
+    print "Working in Process #%d, %d" % (os.getpid(), count)
+    return get_results
+
+
+def mp_map():
+    po = multiprocessing.Pool(8)
+    pool_outputs = po.map(test_search, stub_files)
+    print pool_outputs
+    po.close()
+    po.join()
+
+
+def mp_map_async():
+	  po = multiprocessing.Pool(8)
+	  pool_outputs = po.map_async(test_search, stub_files)
+	  print pool_outputs.get()
+
+
+results = []
+def collect_results(result):
+	  results.append(result)
+
+
+def mp_apply_async():
+    po = multiprocessing.Pool(8)
+    for stub_file in stub_files:
+        po.apply_async(test_search, args=(stub_file, ), callback=collect_results)
+    po.close()
+    po.join()
+    for generator in results:
+        for r in generator():
+        	  print r['terms']
+
+
+if __name__ == '__main__':
+    # mp_map()
+
+    # mp_map_async()
+
+    mp_map_async()
